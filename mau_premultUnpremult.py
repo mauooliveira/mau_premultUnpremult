e848ad56c30f91c929533104fef59fd59231f183
Mau Oliveira <96890754+mauooliveira@users.noreply.github.com>
2021-12-30 22:30:20 +0000

    Create mau_premultUnpremult.py
    # version: 1.0.0
    # last updated: 27.03.21

---
+++ mau_premultUnpremult.py
@@ -0,0 +1,18 @@
+#--------------------------------------------------
+# mau_premultUnpremult.py
+# version: 1.0.0
+# last updated: 27.03.21
+#--------------------------------------------------
+
+import nuke
+
+def premultUnpremult():
+
+	for i in nuke.selectedNodes():
+	    try:
+	        if i.knob("unpremult").value() == "alpha":
+	            i.knob("unpremult").setValue("none")
+	        else:
+	            i.knob("unpremult").setValue("alpha")
+	    except:
+	        pass
